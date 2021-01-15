f7eac3e971026c4e94f765f059d665eaaf76cb24
GriS <emailofgris@gmail.com>
2021-01-15 16:08:20 +0400

    2021.01.15_2

--- app/Mail/OrderShipped.php
+++ app/Mail/OrderShipped.php
@@ -19,10 +19,17 @@ class OrderShipped extends Mailable {
   }
 
   public function build() {
-    return $this->from(env('MAIL_FROM_ADDRESS'))
-    ->view('emails.neworder')
-    ->subject('Заказ '.$this->order->id)
-    ->with(['order' => $this->order, 'products'=>$this->products])
-    ->attach(env('APP_URL').'/public'.str_replace ('public/', '' ,$this->order->requisites));
+    if ($this->order->requisites != null) {
+      return $this->from(env('MAIL_FROM_ADDRESS'))
+        ->view('emails.neworder')
+        ->subject('Заказ ' . $this->order->id)
+        ->with(['order' => $this->order, 'products' => $this->products])
+        ->attach(env('APP_URL') . public_path() . str_replace(public_path(), '', $this->order->requisites));
+    }else{
+      return $this->from(env('MAIL_FROM_ADDRESS'))
+        ->view('emails.neworder')
+        ->subject('Заказ ' . $this->order->id)
+        ->with(['order' => $this->order, 'products' => $this->products]);
+    }
   }
 }
